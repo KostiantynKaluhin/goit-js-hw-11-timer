290060a8e2caa84f8e239f06c1a0818ce84cab52
Kostianyn <odessavshok@gmail.com>
2021-10-25 03:54:42 +0300

    done

--- index.js
+++ index.js
@@ -8,4 +8,40 @@ class Countdown {
     this.mins = this.selector.querySelector('[data-value="mins"]');
     this.secs = this.selector.querySelector('[data-value="secs"]');
   }
+
+  beginning() {
+    this.style();
+    setInterval(() => {
+      let currentTime = Date.now();
+      this.deltaTime = this.targetDate - currentTime;
+      const days = this.pad(Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      const mins = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)),
+      );
+      const secs = this.pad(Math.floor((this.deltaTime % (1000 * 60)) / 1000));
+      this.updateTimer(days, hours, mins, secs);
+    }, 1000);
+  }
+  style() {
+    this.selector.style.display = 'flex';
+    this.selector.style.alignItems = 'center';
+    this.selector.style.justifyContent = 'center';
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  updateTimer(days, hours, mins, secs) {
+    this.days.textContent = `${days}`;
+    this.hours.textContent = `${hours}`;
+    this.mins.textContent = `${mins}`;
+    this.secs.textContent = `${secs}`;
+  }
 }
+const SummerTime = new Countdown({
+  selector: '#timer-1',
+  targetDate: new Date('June 01,2022'),
+});
+SummerTime.beginning();
